b39330cb3e05cc20ef3f9530780d66647d717c89
Paul Shryock <paul@pshry.com>
2020-06-02 14:28:14 -0400

    Serialize form data

--- src/client/_assets/js/components/pledge-form.js
+++ src/client/_assets/js/components/pledge-form.js
@@ -18,6 +18,33 @@ function addSubmitListeners () {
   })
 }
 
+/*!
+ * Serialize all form data into an object of key/value pairs
+ * (c) 2020 Chris Ferdinandi, MIT License, https://gomakethings.com
+ * @param  {Node}   form The form to serialize
+ * @return {Object}      The serialized form data
+ */
+var serializeObject = function (form) {
+  var obj = {}
+  Array.prototype.slice.call(form.elements).forEach(function (field) {
+    if (!field.name || field.disabled || ['file', 'reset', 'submit', 'button'].indexOf(field.type) > -1) return
+    if (field.type === 'select-multiple') {
+      var options = []
+      Array.prototype.slice.call(field.options).forEach(function (option) {
+        if (!option.selected) return
+        options.push(option.value)
+      })
+      if (options.length) {
+        obj[field.name] = options
+      }
+      return
+    }
+    if (['checkbox', 'radio'].indexOf(field.type) >-1 && !field.checked) return
+    obj[field.name] = field.value
+  })
+  return obj
+}
+
 /**
  * Handles pledge form
  */
@@ -28,7 +55,7 @@ function handlePledgeForm (event) {
 
   fetch('/api/pledges', {
     method: 'POST',
-    body: new FormData(event.target),
+    body: JSON.stringify(serializeObject(event.target)),
   }).then(function (response) {
     if (response.ok) {
       return response.json()
