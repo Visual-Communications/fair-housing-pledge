7b76882868df25bbd3f3ee1ff95c3a0b6aca55b5
Paul Shryock <paul@pshry.com>
2020-08-07 17:19:34 -0400

    Fix typo

--- src/client/_content/pages/home.11tydata.js
+++ src/client/_content/pages/home.11tydata.js
@@ -12,8 +12,7 @@ module.exports = {
         title: 'NAR Fair Housing at Home'
       }
     ]
-  }
-},
+  },
   scripts: [
     {
       src: "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js",
