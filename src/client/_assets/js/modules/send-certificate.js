a0fb5e04e432c126cd3c26a7f3b5a923790db2b9
= <Esemplaret1@gmail.com>
2020-08-21 13:09:39 -0400

    Updating image links w/ new tokens

--- src/client/_assets/js/modules/send-certificate.js
+++ src/client/_assets/js/modules/send-certificate.js
@@ -29,14 +29,14 @@ var footerImage = document.getElementById('brandFooterID')
   switch(brand) {
     case "Better Homes and Gardens Real Estate" : {
       console.log("brand: " + brand);
-      headerImage.src = "https://raw.githubusercontent.com/Visual-Communications/fair-housing-pledge/master/src/client/_assets/img/certificate/FH%20Pledge_BHG_H.jpg?token=AIK53GK6X2JZMAJJECSHDB27GXDXG"
-      footerImage.src = "https://raw.githubusercontent.com/Visual-Communications/fair-housing-pledge/master/src/client/_assets/img/certificate/FH%20Pledge_BHG_F.jpg?token=AIK53GMD5Z6MKSMVYCW5WVK7GXDY4"
+      headerImage.src = "https://raw.githubusercontent.com/Visual-Communications/fair-housing-pledge/master/src/client/_assets/img/certificate/FH%20Pledge_BHG_H.jpg?token=AIK53GM7MCW4NHBVKN6BXAK7JE4NG"
+      footerImage.src = "https://raw.githubusercontent.com/Visual-Communications/fair-housing-pledge/master/src/client/_assets/img/certificate/FH%20Pledge_BHG_F.jpg?token=AIK53GK55MSMEFM53UY7RQC7JE4PO"
       break;
     }
     case "Century 21 Real Estate" : {
       console.log("brand: " + brand);
-      headerImage.src = "https://raw.githubusercontent.com/Visual-Communications/fair-housing-pledge/master/src/client/_assets/img/certificate/FH%20Pledge_C21_H.jpg?token=AIK53GOAB3RDMFPJEHDUY4S7GWVFA"
-      footerImage.src = "https://raw.githubusercontent.com/Visual-Communications/fair-housing-pledge/master/src/client/_assets/img/certificate/C21-Footer.jpg?token=AIK53GPQSYKPFXJ2WHAPSSS7GXDJU"
+      headerImage.src = "https://raw.githubusercontent.com/Visual-Communications/fair-housing-pledge/master/src/client/_assets/img/certificate/FH%20Pledge_C21_H.jpg?token=AIK53GIR2LXYGC7SRKXG44C7JE4RA"
+      footerImage.src = "https://raw.githubusercontent.com/Visual-Communications/fair-housing-pledge/master/src/client/_assets/img/certificate/FH%20Pledge_C21_F.jpg?token=AIK53GOPC5YHYFV3JHQWFL27JE4SU"
       break;
     }
     //TODO: Add Coldwell Banker Images
@@ -44,32 +44,32 @@ var footerImage = document.getElementById('brandFooterID')
     case "Coldwell Banker – Affiliates" :
     case "Coldwell Banker Commercial" : {
       console.log("brand: " + brand);
-      headerImage.src = "https://raw.githubusercontent.com/Visual-Communications/fair-housing-pledge/master/src/client/_assets/img/certificate/FH%20Pledge_CB_H.jpg?token=AIK53GISHE2ZBXCJTKF2VYC7G2WK6"
-      footerImage.src = "https://raw.githubusercontent.com/Visual-Communications/fair-housing-pledge/master/src/client/_assets/img/certificate/FH%20Pledge_CB_F.jpg?token=AIK53GI2TSE7G3DGSNAKSMS7G2WPM"
+      headerImage.src = "https://raw.githubusercontent.com/Visual-Communications/fair-housing-pledge/master/src/client/_assets/img/certificate/FH%20Pledge_CB_H.jpg?token=AIK53GPZIB5L2VDDIKV64HS7JE4VQ"
+      footerImage.src = "https://raw.githubusercontent.com/Visual-Communications/fair-housing-pledge/master/src/client/_assets/img/certificate/FH%20Pledge_CB_F.jpg?token=AIK53GJTY5OCHS6N6AB3B7K7JE4XC"
       break;
     }
     case "Corcoran" : {
       console.log("brand: " + brand);
-      headerImage.src = "https://raw.githubusercontent.com/Visual-Communications/fair-housing-pledge/master/src/client/_assets/img/certificate/FH%20Pledge_Corcoran_H.jpg?token=AIK53GJB7VLX4BCLJHQ2XNK7GXDNA"
-      footerImage.src = "https://raw.githubusercontent.com/Visual-Communications/fair-housing-pledge/master/src/client/_assets/img/certificate/FH%20Pledge_Corcoran_F.jpg?token=AIK53GOP4F57AOUXR3FO5427GXDPG"
+      headerImage.src = "https://raw.githubusercontent.com/Visual-Communications/fair-housing-pledge/master/src/client/_assets/img/certificate/FH%20Pledge_Corcoran_H.jpg?token=AIK53GMM2LDSS6OMAG3ZCCC7JE254"
+      footerImage.src = "https://raw.githubusercontent.com/Visual-Communications/fair-housing-pledge/master/src/client/_assets/img/certificate/FH%20Pledge_Corcoran_F.jpg?token=AIK53GKLZ7UAHHDU6T6WLPK7JE3WO"
       break;
     }
     case "ERA Real Estate" : {
       console.log("brand: " + brand);
-      headerImage.src = "https://raw.githubusercontent.com/Visual-Communications/fair-housing-pledge/master/src/client/_assets/img/certificate/FH%20Pledge_ERA_H.jpg?token=AIK53GN4UXHLW3AHKKJDLFS7GXDRO"
-      footerImage.src = "https://raw.githubusercontent.com/Visual-Communications/fair-housing-pledge/master/src/client/_assets/img/certificate/FH%20Pledge_ERA_F.jpg?token=AIK53GICA54W4A3EIJDACSS7GXDTO"
+      headerImage.src = "https://raw.githubusercontent.com/Visual-Communications/fair-housing-pledge/master/src/client/_assets/img/certificate/FH%20Pledge_ERA_H.jpg?token=AIK53GP36XNQNXSA523YK2K7JE4Y4"
+      footerImage.src = "https://raw.githubusercontent.com/Visual-Communications/fair-housing-pledge/master/src/client/_assets/img/certificate/FH%20Pledge_ERA_F.jpg?token=AIK53GP7NAQWYI7UY3PWV3K7JE42M"
       break; 
     }
     case "Sotheby’s International Realty" : {
       console.log("brand: " + brand);
-      headerImage.src = "https://raw.githubusercontent.com/Visual-Communications/fair-housing-pledge/master/src/client/_assets/img/certificate/FH%20Pledge_SIR_H.jpg?token=AIK53GOQHRQTLPQPBUESPH27GXD26"
-      footerImage.src = "https://raw.githubusercontent.com/Visual-Communications/fair-housing-pledge/master/src/client/_assets/img/certificate/FH%20Pledge_SIR_F.jpg?token=AIK53GKPTVF6RLQM63KRXDK7GXD4U"
+      headerImage.src = "https://raw.githubusercontent.com/Visual-Communications/fair-housing-pledge/master/src/client/_assets/img/certificate/FH%20Pledge_SIR_H.jpg?token=AIK53GM3JX2XHC35CNQ2K4C7JE43O"
+      footerImage.src = "https://raw.githubusercontent.com/Visual-Communications/fair-housing-pledge/master/src/client/_assets/img/certificate/FH%20Pledge_SIR_F.jpg?token=AIK53GL4UVHQ43ES56ANKCK7JE44U"
       break; 
     }    
 
     default : {
-      headerImage.src = "https://raw.githubusercontent.com/Visual-Communications/fair-housing-pledge/master/src/client/_assets/img/certificate/FH%20Pledge_Generic_H.jpg?token=AIK53GNHG56S7H7FZTXWTTS7INPHW";
-      footerImage.src = "https://raw.githubusercontent.com/Visual-Communications/fair-housing-pledge/master/src/client/_assets/img/certificate/FH%20Pledge_Generic_F.jpg?token=AIK53GJTW6GNZHSBYPTBBCS7INPSS";
+      headerImage.src = "https://raw.githubusercontent.com/Visual-Communications/fair-housing-pledge/master/src/client/_assets/img/certificate/FH%20Pledge_Generic_H.jpg?token=AIK53GMT6LQLESLLHRKW4YS7JE46G";
+      footerImage.src = "https://raw.githubusercontent.com/Visual-Communications/fair-housing-pledge/master/src/client/_assets/img/certificate/FH%20Pledge_Generic_F.jpg?token=AIK53GJTZCVZ5N7E4OHHO727JE47E";
     }
 
   }
